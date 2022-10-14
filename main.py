deab449184671c5e7cbdd7b7535d25878cdf86b6
sangnv <sangnv@tandan.com.vn>
2022-10-14 16:04:59 +0700

    update

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 from fastapi import FastAPI, File, UploadFile
 from process import ReturnInfoCard
 import json
+import os
+import cv2
+import time
+import shutil
+from starlette.background import BackgroundTasks
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
 @app.post("/CitizenIdentification/upload")
 def upload(file: UploadFile = File(...)):
     try:
         contents = file.file.read()
-        with open(file.filename, 'wb') as f:
-            f.write(contents)
-            obj = ReturnInfoCard(file.filename)
-            if(obj.type=="cccd_front"):
-                return {"id": obj.id,"name": obj.name,"dob": obj.dob,"sex": obj.sex,"nationality":obj.nationality,
-                        "home":obj.home,"address":obj.address,"doe":obj.doe,"type":obj.type,"status": obj.status,"message": obj.message}
-            if(obj.type=="cccd_back"):
-                return {"features": obj.features,"issue_date": obj.issue_date,"type": obj.type,
-                        "status": obj.status,"message": obj.message}
-            else:
-                return {"type": obj.type,"status": obj.status,"message": obj.message}
+        f = open(file.filename, 'wb')
+        f.write(contents)
+        pathSave = os.getcwd() + '\\ImageCard'
+        if (os.path.exists(pathSave)):
+            imgName = file.filename + '_'+str(time.time())+'.jpg'
+            shutil.copyfile(file.filename, pathSave+"\\" + imgName)
+        else:
+            os.mkdir(pathSave)
+            imgName = file.filename + '_'+str(time.time())+'.jpg'
+            shutil.copyfile(file.filename, pathSave+"\\" + imgName)
+        obj = ReturnInfoCard(file.filename)
+        if (obj.type == "cccd_front"):
+            return {"id": obj.id, "name": obj.name, "dob": obj.dob, "sex": obj.sex, "nationality": obj.nationality,
+                    "home": obj.home, "address": obj.address, "doe": obj.doe, "type": obj.type, "status": obj.status, "message": obj.message}
+        if (obj.type == "cccd_back"):
+            return {"features": obj.features, "issue_date": obj.issue_date, "type": obj.type,
+                    "status": obj.status, "message": obj.message}
+        else:
+            return {"type": obj.type, "status": obj.status, "message": obj.message}
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
